ba3623076658cbec1c7a5ee39111f2db5f190ae8
YunYouJun <me@yunyoujun.cn>
2020-11-28 14:13:50 +0800

    :children_crossing: export utils

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ const pkg = require("../package.json");
 
 const program = createCommand();
 program.version(pkg.version);
+
+export * as fs from "./fs";
+export * as http from "./fs";
+export * as utils from "./fs";
