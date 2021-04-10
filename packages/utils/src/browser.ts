0c5691d6f79e7ec56315a99f654ee642b34b65a4
YunYouJun <me@yunyoujun.cn>
2021-04-10 13:45:18 +0800

    :sparkles: add debounce & throttle

---
+++ packages/utils/src/browser.ts
@@ -0,0 +1,40 @@
+/**
+ * 浏览器
+ * @packageDocumentation
+ */
+
+/**
+ * 防抖函数：函数被触发后 n 秒执行回调，n 秒内被触发，则重新计时
+ * @param fn 执行的函数
+ * @param delay ms 延迟执行时间
+ */
+export function debounce(fn: Function, delay = 200) {
+  let timer = null;
+  return () => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    timer = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+}
+
+/**
+ * 节流函数：单位时间内，只有一次触发事件的回调函数执行
+ * @param fn
+ * @param threshold ms 单位时间
+ */
+export function throttle(fn: Function, threshold = 200) {
+  // 第一次立即执行
+  let last = 0;
+  return () => {
+    const now = Date.now();
+    if (now - last > threshold) {
+      fn();
+      last = now;
+    }
+  };
+}
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
+export * as browser from "./browser";
 export * as common from "./common";
 export * as fs from "./fs";
 export * as http from "./http";
