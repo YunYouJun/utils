9252ce5e368891390b753cfd306bc1d5d623a385
YunYouJun <me@yunyoujun.cn>
2021-07-18 20:44:50 +0800

    :sparkles: add markdown utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,5 +2,6 @@ export * as browser from "./browser";
 export * as common from "./common";
 export * as fs from "./fs";
 export * as http from "./http";
+export * as markdown from "./markdown";
 import Logger from "@yunyoujun/logger";
 export { Logger };
---
+++ packages/utils/src/markdown.ts
@@ -0,0 +1,34 @@
+import { logger } from "./logger";
+
+/**
+ * 在标签间插入内容
+ * @param namespace 命名空间
+ * @param content 原内容
+ * @param injectedContent 被插入的内容
+ * @returns
+ */
+function injectContentBetweenTags(
+  namespace: string,
+  content: string,
+  injectedContent: string
+) {
+  const startTag = `<!-- ${namespace}:start -->`;
+  const endTag = `<!-- ${namespace}:end -->`;
+
+  const startIndex = content.indexOf(startTag);
+  const endIndex = content.indexOf(endTag, startIndex);
+
+  if (startIndex === -1 || endIndex === -1) {
+    logger.warning(`Can not find ${startTag} and ${endTag}.`);
+    return "";
+  }
+  return [
+    content.slice(0, startIndex + startTag.length),
+    "\n<!-- prettier-ignore-start -->",
+    "\n<!-- markdownlint-disable -->\n",
+    injectedContent,
+    "\n<!-- markdownlint-restore -->",
+    "\n<!-- prettier-ignore-end -->\n",
+    content.slice(endIndex),
+  ].join("");
+}
