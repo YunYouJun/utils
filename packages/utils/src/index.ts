918419ec3f072e8fdff4106d78ff2107dd05e626
YunYouJun <me@yunyoujun.cn>
2022-04-30 02:34:14 +0800

    refactor: extract http standalone

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,6 +2,5 @@ import { Logger } from '@yunyoujun/logger'
 export * as browser from './browser'
 export * as common from './common'
 export * as fs from './fs'
-export * as http from './http'
 export * as markdown from './markdown'
 export { Logger }
